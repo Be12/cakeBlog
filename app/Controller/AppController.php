fc95bb7d9e3460e71e6eb98c7c01304b61fae8e3
Be12 <johno.power@gmail.com>
2014-03-20 17:19:48 +0000

    johno.power@gmail.com

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -1,38 +1,40 @@
 <?php
 /**
- * Application level Controller
- *
- * This file is application-wide controller file. You can put all
- * application-wide controller-related methods here.
- *
- * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
- *
- * Licensed under The MIT License
- * For full copyright and license information, please see the LICENSE.txt
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @link          http://cakephp.org CakePHP(tm) Project
- * @package       app.Controller
- * @since         CakePHP(tm) v 0.2.9
- * @license       http://www.opensource.org/licenses/mit-license.php MIT License
- */
+* Application level Controller
+*
+* This file is application-wide controller file. You can put all
+* application-wide controller-related methods here.
+*
+* CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+* Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+*
+* Licensed under The MIT License
+* For full copyright and license information, please see the LICENSE.txt
+* Redistributions of files must retain the above copyright notice.
+*
+* @copyright Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+* @link http://cakephp.org CakePHP(tm) Project
+* @package app.Controller
+* @since CakePHP(tm) v 0.2.9
+* @license http://www.opensource.org/licenses/mit-license.php MIT License
+*/
 
 App::uses('Controller', 'Controller');
 
+
 /**
- * Application Controller
- *
- * Add your application-wide methods in the class below, your controllers
- * will inherit them.
- *
- * @package		app.Controller
- * @link		http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
- */
+* Application Controller
+*
+* Add your application-wide methods in the class below, your controllers
+* will inherit them.
+*
+* @package app.Controller
+* @link http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
+*/
 class AppController extends Controller {
 
- public $components = array(
+public $components = array(
+//'DebugKit.Toolbar',
         'Session',
         'Auth' => array(
             'loginRedirect' => array(
@@ -40,33 +42,18 @@ class AppController extends Controller {
                 'action' => 'index'
             ),
             'logoutRedirect' => array(
-                'controller' => 'pages',
-                'action' => 'display',
-                'home'
-            )
+                'controller' => 'users',
+                'action' => 'login',
+            ),
+            'authorize' => array('Controller')
         )
     );
 
-    public function beforeFilter() {
-        $this->Auth->allow('index', 'view');
-    }
+public function beforeFilter(){
+$this->Auth->allow('index','logout', 'display');
+}
 
- }
-
- public $components = array(
-    'Session',
-    'Auth' => array(
-        'loginRedirect' => array('controller' => 'posts', 'action' => 'index'),
-        'logoutRedirect' => array(
-            'controller' => 'pages',
-            'action' => 'display',
-            'home'
-        ),
-        'authorize' => array('Controller') // Added this line
-    )
- );
-
- public function isAuthorized($user) {
+public function isAuthorized($user) {
     // Admin can access every action
     if (isset($user['role']) && $user['role'] === 'admin') {
         return true;
@@ -74,6 +61,6 @@ class AppController extends Controller {
 
     // Default deny
     return false;
-  }
-
 }
+}
+?>
